ecc00b91de925e18e12d6baba10e928e763338bc
abr-ya <gamer05@list.ru>
2020-09-12 16:15:26 +0300

    Add async in Hello function

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,8 +1,6 @@
-exports.handler = (event, context, callback) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      msg: 'Hello World',
-    }),
-  };
-};
+exports.handler = async (event, context, callback) => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    msg: 'Hello World',
+  }),
+});
